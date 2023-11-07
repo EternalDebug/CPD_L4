f2f2ffeb7dcdaddee26609edff77705b3c19a852
EternalDebug <71968876+EternalDebug@users.noreply.github.com>
2023-11-07 13:29:11 +0300

    Add files via upload

---
+++ spaceXreact-master/src/components/launchList.jsx
@@ -0,0 +1,66 @@
+import * as d3 from "d3";
+import { containerRef } from "./map";
+import { g } from "./map";
+import { projection } from "./map";
+//import { zoom } from "./map";
+
+
+let launches;
+let launchpads;
+function LaunchList(props) {
+    launches = props.launches
+    launchpads = props.launchpads
+    return (
+        <aside className="aside" id="launchesContainer">
+            <h3>Launches</h3>
+            <div id="listContainer" >
+
+
+                <ul>
+                    {launches.map(launch => {
+                        return <li onMouseEnter={MakeColor} onMouseLeave={NoColor} key={launch.id}>{launch.name} </li>
+                    })}
+                </ul>
+            </div>
+        </aside>
+    )
+
+    function MakeColor(Event) {
+        let Elem = Event.target
+        Elem.style.backgroundColor = 'red';
+        let text = Elem["innerText"]
+        SwithPointColor(text, 'orange')
+    }
+    function NoColor(Event) {
+        let Elem = Event.target
+        Elem.style.backgroundColor = 'white';
+        let text = Elem["innerText"]
+        SwithPointColor(text, 'blue')
+    }
+
+    function SwithPointColor(text, color) {
+        launches.map(Launche => {
+            if (Launche.name === text) {
+                let launchpad_UUID = Launche.launchpad
+                launchpads.map(Launchpad => {
+                    if (Launchpad.id === launchpad_UUID) {
+
+                        let x = Launchpad.longitude
+                        let y = Launchpad.latitude
+
+                        let [xx, yy] = projection([x, y])
+                        svg = d3.select(containerRef.current).select("svg")
+                        g.append("circle")
+                            .attr("r", 5)
+                            .attr("cx", xx)
+                            .attr("cy", yy)
+                            .style("fill", color)
+                            .attr("class", "topo")
+                    }
+                })
+            }
+        })
+    }
+}
+
+export { LaunchList }
